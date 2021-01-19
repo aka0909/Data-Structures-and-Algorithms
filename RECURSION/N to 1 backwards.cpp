1fd2ddf74cf434e68276bac0a1880df11c7fc16e
aka0909 <65956313+aka0909@users.noreply.github.com>
2021-01-19 17:17:58 +0530

    Create N to 1 backwards.cpp

---
+++ RECURSION/N to 1 backwards.cpp
@@ -0,0 +1,24 @@
+#Recursive Solution
+#Time Complexity: O(n)
+#Auxiliary Space: O(n)
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void fun(int n)
+{
+   if(n==1)
+     cout<<1;
+   else
+   {
+       cout<<n<<" ";
+       fun(n-1);
+   }
+}
+int main()
+{
+    int x{};
+    cin>>x;
+    fun(x);
+    return 0;
+}
