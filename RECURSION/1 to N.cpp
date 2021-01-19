0e2ede5be038675bee8f0fd5381c970f8070bd5e
aka0909 <65956313+aka0909@users.noreply.github.com>
2021-01-19 17:23:21 +0530

    Create 1 to N.cpp

---
+++ RECURSION/1 to N.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void fun(int n)
+{
+   #Recursive solution
+   #Time Complexity: O(n)
+   #Auxiliary Space: O(n)
+   if(n==1)
+     cout<<1<<" ";
+   else
+   {
+       fun(n-1);
+       cout<<n<<" ";
+   }
+}
+int main()
+{
+    int x{};
+    cin>>x;
+    fun(x);
+    return 0;
+}
