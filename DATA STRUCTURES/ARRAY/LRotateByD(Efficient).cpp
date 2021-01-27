d5d062ad99067b7bce7193db065143dcd962acc7
aka0909 <65956313+aka0909@users.noreply.github.com>
2021-01-28 01:20:22 +0530

    Create LRotateByD(Efficient).cpp

---
+++ DATA STRUCTURES/ARRAY/LRotateByD(Efficient).cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void leftRotate(int arr[],int n,int d)
+{
+    int temp[d];
+   for(int i=0;i<d;i++)
+   {
+       temp[i]=arr[i];
+   }
+   for(int i=d;i<n;i++)
+   {
+       arr[i-d]=arr[i];
+   }
+   for(int i=0;i<d;i++)
+   {
+       arr[n-d+i]=temp[i];
+   }
+}
+int main()
+{
+      int arr[] = {1, 2, 3, 4, 5}, n = 5, d = 2;
+
+      cout<<"Before Rotation"<<endl;
+
+       for(int i = 0; i < n; i++)
+       {
+       		cout<<arr[i]<<" ";
+       }
+
+       cout<<endl;
+
+       leftRotate(arr, n, d);
+
+       cout<<"After Rotation"<<endl;
+
+       for(int i = 0; i < n; i++)
+       {
+       		cout<<arr[i]<<" ";
+       }
+}
