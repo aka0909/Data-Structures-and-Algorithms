e5ed7b1a5008d14e1b2fb6daf0cebe474fc5c794
aka0909 <65956313+aka0909@users.noreply.github.com>
2021-04-30 16:08:53 +0530

    Create BFS_TYPE_1.cpp

---
+++ ALGORITHMS/GRAPHS/BFS_TYPE_1.cpp
@@ -0,0 +1,60 @@
+//FIRST VERSION OF BFS:
+//Given an undirected graph and a source vertex 's'print BFS from the given source
+
+#include <bits/stdc++.h>
+#include <queue>
+using namespace std;
+
+
+void addEdge(vector<int>adj[],int u, int v)
+{
+    adj[u].push_back(v);
+    adj[v].push_back(u);
+}
+
+void printGraph(vector<int> adj[],int v)
+{
+    for(int i=0;i<v;i++)
+    {
+        for(int x:adj[i])
+            cout<<x<<" ";
+        cout<<endl;
+    }
+}
+
+void BFS(vector<int> adj[],int v,int s)
+{
+    bool visited[v+1];
+    for(int i=0;i<v;i++)
+        visited[i]=false;
+    queue<int>q;
+    visited[s]=true;
+    q.push(s);
+    while(!q.empty())
+    {
+        int u=q.front();
+        q.pop();
+        cout<<u<<" ";
+        for(int v:adj[u])
+        {
+            if(visited[v]==false)
+            {
+                visited[v]=true;
+                q.push(v);
+            }
+        }
+    }
+}
+
+int main()
+{
+    int v=4;
+    vector<int> adj[v];
+    addEdge(adj,0,1);
+    addEdge(adj,0,2);
+    addEdge(adj,1,2);
+    addEdge(adj,1,3);
+    BFS(adj,v,s);
+    return 0;
+}
+
