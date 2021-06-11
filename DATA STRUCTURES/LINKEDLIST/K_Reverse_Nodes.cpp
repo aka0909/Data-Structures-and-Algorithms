49f1630cf6b960832b37819f5eaa77ed11c67853
aka0909 <65956313+aka0909@users.noreply.github.com>
2021-06-11 11:24:18 +0530

    Create K_Reverse_Nodes.cpp

---
+++ DATA STRUCTURES/LINKEDLIST/K_Reverse_Nodes.cpp
@@ -0,0 +1,132 @@
+// { Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+struct node
+{
+    int data;
+    struct node* next;
+    
+    node(int x){
+        data = x;
+        next = NULL;
+    }
+    
+};
+
+/* Function to print linked list */
+void printList(struct node *node)
+{
+    while (node != NULL)
+    {
+        printf("%d ", node->data);
+        node = node->next;
+    }
+    printf("\n");
+}
+
+
+ // } Driver Code Ends
+/*
+  Reverse a linked list
+  The input list will have at least one element  
+  Return the node which points to the head of the new LinkedList
+  Node is defined as 
+    struct node
+    {
+        int data;
+        struct node* next;
+    
+        node(int x){
+            data = x;
+            next = NULL;
+        }
+    
+    }*head;
+*/
+
+class Solution
+{
+    public:
+    struct node *reverse (struct node *head, int k)
+    { 
+        if(head==NULL)return head;
+        stack<node*> st;
+        struct node *current=head;
+        struct node *prev=NULL;
+        int cnt=0;
+        while(current)
+        {
+            int cnt=0;
+            while(current && cnt<k)
+            {
+                st.push(current);
+                current=current->next;
+                cnt++;
+            }
+            while(!st.empty())
+            {
+                if(prev==NULL){
+                    prev=st.top();
+                    head=prev;
+                    st.pop();
+                }
+                else
+                {
+                    prev->next=st.top();
+                    prev=prev->next;
+                    st.pop();
+                }
+            }
+            
+        }
+        prev->next=NULL;
+        return head;
+    }
+};
+
+
+// { Driver Code Starts.
+
+/* Drier program to test above function*/
+int main(void)
+{
+    int t;
+    cin>>t;
+     
+    while(t--)
+    {
+        struct node* head = NULL;
+        struct node* temp = NULL;
+        int n;
+        cin >> n;
+         
+        for(int i=0 ; i<n ; i++)
+        {
+            int value;
+            cin >> value;
+            if(i == 0)
+            {
+                head = new node(value);
+                temp = head;
+            }
+            else
+            {
+                temp->next = new node(value);
+                temp = temp->next;
+            }
+        }
+        
+        int k;
+        cin>>k;
+        
+        Solution ob;
+        head = ob.reverse(head, k);
+        printList(head);
+    }
+     
+    return(0);
+}
+
+  // } Driver Code Ends
