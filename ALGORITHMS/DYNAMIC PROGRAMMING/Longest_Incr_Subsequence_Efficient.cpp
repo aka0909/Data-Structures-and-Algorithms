0ab0cca13855c0d63b247de657b4c6a74e9bd13b
aka0909 <65956313+aka0909@users.noreply.github.com>
2021-06-28 15:48:36 +0530

    Create Longest_Incr_Subsequence_Efficient.cpp

---
+++ ALGORITHMS/DYNAMIC PROGRAMMING/Longest_Incr_Subsequence_Efficient.cpp
@@ -0,0 +1,42 @@
+// { Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+class Solution {
+	public:
+		int AlternatingaMaxLength(vector<int>&nums){
+		    int n=nums.size();
+		    int res=1;
+		    int inc=1,dec=1;
+		    for(int i=1;i<n;i++)
+		    {
+		        if(nums[i]>nums[i-1])
+		         inc=dec+1;
+		        else if(nums[i]<nums[i-1])
+		         dec=inc+1;
+		    }
+		    
+		    res=max(inc,dec);
+		    
+		    return res;
+		}
+
+};
+
+// { Driver Code Starts.
+int main(){
+	int tc;
+	cin >> tc;
+	while(tc--){
+		int n;
+		cin >> n;
+		vector<int>nums(n);
+		for(int i = 0; i < n; i++)
+			cin >> nums[i];
+		Solution obj;
+		int ans = obj.AlternatingaMaxLength(nums);
+		cout << ans << "\n";
+	}
+	return 0;
+}  // } Driver Code Ends
