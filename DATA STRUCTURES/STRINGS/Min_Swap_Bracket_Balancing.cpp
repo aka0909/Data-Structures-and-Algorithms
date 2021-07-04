99961a708155914c0918ce8a25e86c38ab354e3f
aka0909 <65956313+aka0909@users.noreply.github.com>
2021-07-04 11:06:31 +0530

    Update Min_Swap_Bracket_Balancing.cpp

--- DATA STRUCTURES/STRINGS/Min_Swap_Bracket_Balancing.cpp
+++ DATA STRUCTURES/STRINGS/Min_Swap_Bracket_Balancing.cpp
@@ -1,45 +1,33 @@
-long swapCount(string chars) 
-{
-     
-    // Stores total number of Left and 
-    // Right brackets encountered
-    int countLeft = 0, countRight = 0; 
-     
-    // swap stores the number of swaps 
-    // required imbalance maintains 
-    // the number of imbalance pair
-    int swap = 0 , imbalance = 0; 
-      
-    for(int i = 0; i < chars.length(); i++) 
-    {
-        if (chars[i] == '[') 
+class Solution{   
+public:
+    int minimumNumberOfSwaps(string S){
+        
+        int cnt_left=0,cnt_right=0;
+        
+        int swap=0,imbalance=0;
+        
+        for(int i=0;i<S.length();i++)
         {
-             
-            // Increment count of Left bracket
-            countLeft++; 
-             
-            if (imbalance > 0) 
+            if(S[i]=='[')
             {
-                 
-                // swaps count is last swap count + total 
-                // number imbalanced brackets
-                swap += imbalance; 
-                 
-                // imbalance decremented by 1 as it solved
-                // only one imbalance of Left and Right
-                imbalance--;     
+                cnt_left++;
+                
+                if(imbalance>0)
+                {
+                    swap+=imbalance;
+                    
+                    imbalance--;
+                }
             }
-        } 
-        else if(chars[i] == ']' ) 
-        {
-             
-            // Increment count of Right bracket
-            countRight++; 
-             
-            // imbalance is reset to current difference 
-            // between Left and Right brackets
-            imbalance = (countRight - countLeft); 
+            
+            else
+            {
+                cnt_right++;
+                
+                imbalance=cnt_right-cnt_left;
+            }
         }
+        
+        return swap;
     }
-    return swap;
-}
+};
