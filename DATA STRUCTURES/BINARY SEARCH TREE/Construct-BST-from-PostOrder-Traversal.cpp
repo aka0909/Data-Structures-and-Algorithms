28bb150ea0a1a1e031cbadd218d17dea26d8c668
aka0909 <65956313+aka0909@users.noreply.github.com>
2021-07-04 09:46:57 +0530

    Create Construct-BST-from-PostOrder-Traversal.cpp

---
+++ DATA STRUCTURES/BINARY SEARCH TREE/Construct-BST-from-PostOrder-Traversal.cpp
@@ -0,0 +1,36 @@
+//Function that constructs BST from its preorder traversal.
+Node* constructTree(int pre[], int size)
+{
+  stack<Node*> st;
+  
+  Node* root = newNode(pre[0]);
+  st.push(root);
+  
+  Node* temp=NULL;
+  
+  for(int i=1;i<size;i++)
+  {
+      temp=NULL;
+      
+      while(!st.empty() && pre[i]>st.top()->data)
+      {
+          temp=st.top();
+          st.pop();
+      }
+      
+      if(temp)
+      {
+          temp->right=newNode(pre[i]);
+          st.push(temp->right);
+      }
+      
+      else
+      {
+          (st.top())->left=newNode(pre[i]);
+          st.push((st.top())->left);
+      }
+      
+  }
+  return root;
+    
+}
