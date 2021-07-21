4f76d066f25d528093e3e1330bc05c5eb2d500bd
aka0909 <65956313+aka0909@users.noreply.github.com>
2021-07-21 16:51:24 +0530

    Create Bubble_Sort.cpp

---
+++ ALGORITHMS/SORTING/Bubble_Sort.cpp
@@ -0,0 +1,36 @@
+// C++ program of bubble sort
+#include <bits/stdc++.h>
+using namespace std;
+
+
+void print(int arr[], int n){
+    for(int i=0; i<n; i++)
+        cout << arr[i] << " ";
+}
+
+void BubbleSort(int arr[],int n)
+{
+    for(int i=0;i<n-1;i++)
+    {
+        bool swapped=false;
+        for(int j=i+1;j<n-1;j++)
+        {
+            if(arr[i]>arr[j])
+            {swap(arr[i],arr[j]);swapped=true;}
+        }
+        if(swapped==false)
+            break;
+    }
+}
+int main()
+{
+	int arr[10]={56,90,81,72,60,1,23,49,3,100};
+	cout<<"Given Array is: ";
+	print(arr,10);
+	cout<<endl;
+	BubbleSort(arr,10);
+	cout<<"Sorted Array is: ";
+	print(arr,10);
+
+	return 0;
+}
